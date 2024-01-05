905a914f806ee98d63ad64e343131aa4cd9218e0
li-sadeghi <a.sadeghiali@yahoo.com>
2024-01-05 09:40:44 -0500

    solve problems of task-5-6

--- advertiser_mangement/views.py
+++ advertiser_mangement/views.py
@@ -3,16 +3,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Advertiser, Ad, Click, View
 from .forms import InputForm
 from django.contrib import messages
-from django.db.models import Count
-from django.views.generic import TemplateView
+from django.db.models import Count, ExpressionWrapper, F, fields, Sum, Q
+from django.views.generic import TemplateView, RedirectView
 
 
 class HomePageView(TemplateView):
     template_name = 'blog/home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
 
 
 class ShowAdsView(TemplateView):
@@ -20,32 +16,26 @@ class ShowAdsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         ads_approved = Ad.objects.filter(approve=True)
+        new_views = []
         for ad in ads_approved:
             new_view = View(ad=ad, viewer_ip=self.request.user_ip)
-            new_view.save()
+            new_views.append(new_view)
+        View.objects.bulk_create(new_views)
         context = super().get_context_data(**kwargs)
         context['advertisers'] = Advertiser.objects.all()
         return context
 
 
-class ClickView(TemplateView):
-    template_name = "ads/ad.html"
+class ClickView(RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'click-ad'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["advertisers"] = Ad.objects.all()
-        context["error_message"] = "You didn't select an Ad."
-        return context
-
-    def get(self, request, ad_id):
-        ad = get_object_or_404(Ad, pk=ad_id)
-
-        try:
-            click_event = Click(ad=ad, clicker_ip=request.user_ip)
-            click_event.save()
-            return redirect(ad.link)
-        except (KeyError, Ad.DoesNotExist):
-            return self.render_to_response(self.get_context_data())
+    def get_redirect_url(self, *args, **kwargs):
+        ad = get_object_or_404(Ad, pk=kwargs['pk'])
+        click_event = Click(ad=ad, clicker_ip=self.request.user_ip)
+        click_event.save()
+        return redirect(ad.link)
 
 
 class AdCreateView(TemplateView):
@@ -88,45 +78,31 @@ class AdsInformationView(TemplateView):
             Ad.objects.annotate(
                 click_count=Count('click', distinct=True),
                 view_count=Count('view', distinct=True),
+                total_diff=Sum(
+                    ExpressionWrapper(
+                        F('view__time') - F('click__time_clicked'),
+                        output_field=fields.DurationField(),
+                    ),
+                    filter=Q(view__viewer_ip=F('click__clicker_ip')),
+                ),
             )
-            .values('id', 'title', 'click_count', 'view_count')
+            .annotate(
+                avg_time_diff=ExpressionWrapper(
+                    F('total_diff') / F('view_count'),
+                    output_field=fields.DurationField(),
+                ),
+                click_rate=ExpressionWrapper(F('click_count') / F('view_count'),
+                                             output_field=fields.DecimalField(),
+                                             )
+            )
+            .values('id', 'title', 'click_count', 'view_count', 'click_rate', 'avg_time_diff')
             .order_by('-view_count')
         )
-
-        for ad_stat in ad_stats:
-            click_count = ad_stat.get('click_count')
-            view_count = ad_stat.get('view_count')
-            rate = 0 if view_count == 0 else click_count/view_count
-            ad_stat['click_rate'] = round(rate, 2)
-            ad_stat['avg_time_diff'] = self.calculate_avg_time_diff(
-                ad_stat['id'])
 
         context = {
             'ad_stats': ad_stats,
         }
         return context
-
-    def calculate_avg_time_diff(self, ad_id):
-        all_views = View.objects.filter(ad_id=ad_id)
-        all_clicks = Click.objects.filter(ad_id=ad_id)
-        all_ip_addresses = View.objects.values('viewer_ip').distinct()
-        total_diff = 0
-        count = 0
-
-        for ip in all_ip_addresses:
-            ip = ip['viewer_ip']
-            view_time = all_views.filter(viewer_ip=ip).order_by('time')
-            click_time = all_clicks.filter(
-                clicker_ip=ip).order_by('time_clicked')
-            if view_time and click_time:
-                total_diff += (view_time.last().time -
-                               click_time.last().time_clicked).total_seconds()
-                count += 1
-
-        if count == 0:
-            return 0
-
-        return round(total_diff / count, 2)
 
 
 home_view = HomePageView.as_view()
